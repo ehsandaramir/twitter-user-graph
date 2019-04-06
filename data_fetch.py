e1d7d01e5c79c83d6ab401e87c775f3840941a2b
Amir Ehsandar <ehsandaramir@gmail.com>
2019-04-07 00:14:21 +0430

    update models to prevent duplicate users and duplicate params

--- data_fetch.py
+++ data_fetch.py
@@ -5,7 +5,8 @@ import json
 from api_data import ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 from models import *
 
-if __name__ == '__main__':
+
+def create_api():
     api = twitter.Api(
         CONSUMER_KEY,
         CONSUMER_SECRET,
@@ -14,9 +15,8 @@ if __name__ == '__main__':
         tweet_mode='extended',
         # sleep_on_rate_limit=True,
     )
+    return api
 
-    seed_screen_name = 'DevCampIUST'
-    seed = TwitterUser.create_by_sn(api, seed_screen_name)
-    # friends = [TwitterUser.create_by_id(api, user_id) for user_id in seed.following_ids]
 
-    print('')
+seed_screen_name = 'DevCampIUST'
+
--- models.py
+++ models.py
@@ -3,53 +3,73 @@ import twitter
 
 
 class TwitterUser:
-    def __init__(self, api: twitter.Api, twitter_user):
-        self.api = api
+    all_users = []
+    api = None
+
+    def __init__(self, twitter_user):
+        TwitterUser.all_users.append(self)
+
         self.user = twitter_user
         self.screen_name = twitter_user.screen_name
         self.id = twitter_user.id
+
         self.following_count = twitter_user.friends_count
         self.follower_count = twitter_user.followers_count
         self.following_ids = []
         self.follower_ids = []
 
-        self.fill_follow_ids()
-        self.tweets = api.GetUserTimeline(screen_name=self.screen_name)
+        self.relation_to = []
+        self.relation_from = []
 
+        self.fill_follow_ids()
 
     def fill_follow_ids(self):
         self.following_ids = self.api.GetFriendIDs(user_id=self.id)
         self.follower_ids = self.api.GetFollowerIDs(user_id=self.id)
 
     @classmethod
-    def create_by_sn(cls, api: twitter.Api, screen_name: str):
-        tmp = api.GetUser(screen_name=screen_name)
-        return cls(api, tmp)
+    def create_instance(cls, twitter_user):
+        for user in cls.all_users:
+            if user.screen_name == twitter_user.screen_name:
+                return user
+        return cls(twitter_user)
 
     @classmethod
-    def create_by_id(cls, api: twitter.Api, user_id: str):
-        tmp = api.GetUser(user_id=user_id)
-        return cls(api, tmp)
+    def create_by_sn(cls, screen_name: str):
+        tmp = cls.api.GetUser(screen_name=screen_name)
+        return cls.create_instance(tmp)
 
-    def generate_relations(self):
-        pass
+    @classmethod
+    def create_by_id(cls, user_id: str):
+        tmp = cls.api.GetUser(user_id=user_id)
+        return cls.create_instance(tmp)
 
     def to_dict(self):
         return {
             'id': self.id,
             'sn': self.screen_name,
-            'fr_count': self.follower_count,
-            'fg_count': self.following_count,
+            'follower_count': self.follower_count,
+            'following_count': self.following_count,
         }
 
 
 class Relation:
-    def __init__(self, api: twitter.Api, from_user: TwitterUser, to_user: TwitterUser):
-        self.api = api
+    api = None
+
+    def __init__(self, from_user: TwitterUser, to_user: TwitterUser):
         self.from_user = from_user
         self.to_user = to_user
         self.weight = 0
+
+        from_user.relation_to.append(self)
+        to_user.relation_from.append(self)
+
         self.determine_weight()
+
+    @classmethod
+    def create_by_user_and_id(cls, user_from: TwitterUser, user_to: int):
+        fetch_user = TwitterUser.create_by_id(user_to.__str__())
+        return cls(user_from, fetch_user)
 
     def determine_weight(self):
         self.weight = 0
@@ -57,3 +77,8 @@ class Relation:
             self.weight += 1
         if self.to_user.id in self.from_user.following_ids:
             self.weight += 1.3
+
+
+def configure_models(api: twitter.Api):
+    TwitterUser.api = api
+    Relation.api =api
